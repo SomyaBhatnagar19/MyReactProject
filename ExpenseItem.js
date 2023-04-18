93490c7a1a0784415f56c8652dd86230911694fa
SomyaBhatnagar19 <122341588+SomyaBhatnagar19@users.noreply.github.com>
2023-04-18 13:29:33 +0530

    This has an array where expenses happen.

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,8 +1,19 @@
+import './ExpenseItem.css';
 function ExpenseItems() {
-  return (<div className='Title'><h2>Expense Item: </h2>
-      <div className='items'> <p>Food for Rs 10 </p> </div>
-      <div className='items'> <p>Petrol for Rs 100</p></div>
-      <div className='items'> <p>Movie for Rs 200</p></div>
+    const LocationOfExpenditure = ['Food', 'Petrol', 'Movie'];
+  return (<div classname='Card'> 
+      <div className='Title'>
+        <h2>Expense Item : </h2>
+      </div>
+      <div className='items'> 
+        <p>{LocationOfExpenditure[0]} for Rs 10 </p> 
+      </div>
+      <div className='items'> 
+        <p>{LocationOfExpenditure[1]} for Rs 100</p>
+      </div>
+      <div className='items'> 
+        <p>{LocationOfExpenditure[2]} for Rs 200</p>
+      </div>
   </div>)
 }
 export default ExpenseItems;
