7735b719b5c3ee924cfe0046e5fc1a7783d07891
SomyaBhatnagar19 <122341588+SomyaBhatnagar19@users.noreply.github.com>
2023-04-18 12:49:08 +0530

    Adding expense items in the component.
    Lets get started with react task-7

---
+++ ExpenseItem.js
@@ -0,0 +1,8 @@
+function ExpenseItems() {
+  return (<div className='Title'><h2>Expense Item: </h2>
+      <div className='items'> <p>Food for Rs 10 </p> </div>
+      <div className='items'> <p>Petrol for Rs 100</p></div>
+      <div className='items'> <p>Movie for Rs 200</p></div>
+  </div>)
+}
+export default ExpenseItems;
