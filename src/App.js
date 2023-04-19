7da4d408641bfd958b955c944e006cf27171bbdf
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-04-19 13:12:05 +0530

    added component folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-
+//importing the custom component we made
+import ExpenseItem from './components/ExpenseItem';
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <ExpenseItem>
+          <h4>Food Rs 10</h4>
+          <h4>Petrol Rs 100</h4>
+          <h4>Movies Rs 200</h4>
+        </ExpenseItem>
         <a
           className="App-link"
           href="https://reactjs.org"
