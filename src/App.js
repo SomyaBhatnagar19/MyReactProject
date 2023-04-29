e5c2018427d54b6e761175fcf1e6692b8bce1d0d
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-04-29 11:40:38 +0530

    Adding update amount but updates all

--- src/App.js
+++ src/App.js
@@ -7,19 +7,19 @@ const App = () => {
     {
       id: "e1",
       title: "Food",
-      amount: "Rs. 10",
+      amount: "Rs. 50",
       date: new Date(2023, 7, 14),
     },
     {
       id: "e2",
       title: "Petrol",
-      amount: "Rs. 50",
-      date: new Date(2021, 2, 12),
+      amount: "Rs. 100",
+      date: new Date(2022, 2, 12),
     },
     {
       id: "e3",
       title: "Movie",
-      amount: "Rs. 100",
+      amount: "Rs. 250",
       date: new Date(2021, 2, 28),
     },
   ];
@@ -30,21 +30,6 @@ const App = () => {
         Expense List:-
       </h5>
       <ExpenseItem location = {LocationOfExpense}/>
-      {/* <ExpenseItem
-        title={LocationOfExpense[0].title}
-        amount={LocationOfExpense[0].amount}
-        date={LocationOfExpense[0].date}
-      />
-      <ExpenseItem
-        title={LocationOfExpense[1].title}
-        amount={LocationOfExpense[1].amount}
-        date={LocationOfExpense[1].date}
-      />
-      <ExpenseItem
-        title={LocationOfExpense[2].title}
-        amount={LocationOfExpense[2].amount}
-        date={LocationOfExpense[2].date}
-      /> */}
     </Card>
     
   );
--- src/CustomComponents/Expenses/ExpenseItem.js
+++ src/CustomComponents/Expenses/ExpenseItem.js
@@ -1,24 +1,58 @@
 import ExpenseDate from "./ExpenseDate";
+import React, { useState } from 'react';
 import "./ExpenseItem.css";
 import ExpenseDetail from "./ExpenseDetail";
 function ExpenseItem(props) {
+
+  const [getTitle, setTitle] = useState(props.location.title);
+  const clickHandler = () => { 
+    setTitle('Updated Title');
+    console.log(getTitle);
+  }
+
   const deleteTitleHandler = (e) => {
     e.preventDefault();
     let expenseId = e.target.dataset.id;
     let expenseElement = document.getElementById(expenseId);
-    if(expenseElement){
+    if (expenseElement) {
       expenseElement.remove();
     }
   };
+
+  const [amount, setAmount] = useState(props.amount);
+  const UpdateExpenseHandler = () => {
+      setAmount('$100');
+      console.log({amount});
+    }
+ 
   return (
     <div>
       {props.location.map((element) => {
         return (
           <div className="expense-item" id={element.id}>
             <ExpenseDate date={element.date} />
-            <ExpenseDetail title={element.title} />
-            <ExpenseDetail amount={element.amount} />
-            <button type='submit' data-id={element.id} onClick={deleteTitleHandler}>Delete Expense</button>
+            <ExpenseDetail title={getTitle} />
+            <ExpenseDetail amount={amount} />
+            <button
+              className="submitBtn"  
+              type = "submit"
+              data-id = {element.id}
+              onClick = {deleteTitleHandler}
+            >
+              Delete Expense
+            </button>
+            <button
+                className="ChangeTitle"
+                onClick = {clickHandler}
+            >
+              Change Title
+            </button>
+            <button
+              className="updateBtn"
+              onClick = {UpdateExpenseHandler}
+            >
+              Update Amount
+              </button>
           </div>
         );
       })}
