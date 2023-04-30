e2f75d556a43b5457f7d3c468095c7675258a958
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-04-30 17:37:24 +0530

    using state in form

--- src/CustomComponents/NewExpense/ExpenseForm.js
+++ src/CustomComponents/NewExpense/ExpenseForm.js
@@ -1,29 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import "./ExpenseForm.css";
 const ExpenseForm = () => {
+  //defining states
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
   const handleTitleChange = (event) => {
-    console.log(event.target.value);
+    setEnteredTitle(event.target.value);
   };
 
   const handleAmountChange = (event) => {
-    console.log(event.target.value);
+    setEnteredAmount(event.target.value);
   };
 
   const handleDateChange = (event) => {
-    console.log(event.target.value);
+    setEnteredDate(event.target.value);
   };
   //form for input
   return (
     <Card>
       <h5>Add Expenses:- </h5>
       <hr></hr>
+      <div>
+        
+      </div>
       <form>
         <label htmlFor="title">Title : </label> &nbsp;
-        <input type="text" id="title" placeholder="Expense Title" onChange={handleTitleChange}/>
+        <input type="text" id="title" placeholder="Expense Title" onChange={handleTitleChange} value={enteredTitle}/>
         &nbsp;&nbsp;
         <label htmlFor="amount">Amount : </label> &nbsp;
-        <input type="number" id="amount" placeholder="Select Expense Amount" onChange={handleAmountChange}/>
+        <input type="number" id="amount" placeholder="Select Expense Amount" onChange={handleAmountChange} value={enteredAmount}/>
         &nbsp;&nbsp;
         <label htmlFor="date">Date : </label> &nbsp;
         <input
@@ -32,6 +39,7 @@ const ExpenseForm = () => {
           placeholder="Select Expense Date"
           min="2023-01-01"
           onChange={handleDateChange}
+          value={enteredDate}
         />
         <br></br>
         <button type="submit" className="form-submit">
